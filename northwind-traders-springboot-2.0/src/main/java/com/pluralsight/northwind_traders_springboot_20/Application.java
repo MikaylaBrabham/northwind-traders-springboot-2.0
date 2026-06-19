d4bc598d969db078742344dfb49a73287e9f2ac0
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-19 16:36:51 -0500

    Initial commit

---
+++ northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind_traders_springboot_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
