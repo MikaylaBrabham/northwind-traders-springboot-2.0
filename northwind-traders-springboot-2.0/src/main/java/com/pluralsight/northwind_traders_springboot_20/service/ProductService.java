44109a680d99c6ca74f514266e353622107c3779
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-19 18:23:33 -0500

    add productservice for service

--- northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/service/ProductService.java
+++ northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/service/ProductService.java
@@ -1,4 +1,25 @@
 package com.pluralsight.northwind_traders_springboot_20.service;
 
+import org.springframework.stereotype.Service;
+
+//announces to spring it's a bean
+@Service
 public class ProductService {
+
+    //add private final repo
+
+    //create constructor injection for repo to get dependencies
+
+
+    //each operation has its own method based on what website does
+
+    // get - read
+    // post - create
+    // put - update
+    // delete - remove
+
+    private ProductService
+
+
+
 }
