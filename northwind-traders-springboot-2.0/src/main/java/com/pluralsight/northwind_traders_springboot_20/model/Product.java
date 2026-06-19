0b11b0c627f2f822432f3ec56ae7b7512f47066e
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-19 17:33:50 -0500

    add product getters

--- northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/model/Product.java
+++ northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/model/Product.java
@@ -4,14 +4,33 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "products")
-
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+    //get variables
     private Integer productID;
     private String name;
     private String category;
     private Double price;
+
+
+    //add getters
+
+    public Integer getProductID() {
+        return productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
