4e6f6faece1c2372d6b528d2e0e2850ffbf068e4
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-19 18:39:10 -0500

    fixed the service to call the repository

--- northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/service/ProductService.java
+++ northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/service/ProductService.java
@@ -1,25 +1,46 @@
 package com.pluralsight.northwind_traders_springboot_20.service;
 
+import com.pluralsight.northwind_traders_springboot_20.model.Product;
+import com.pluralsight.northwind_traders_springboot_20.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
 
 //announces to spring it's a bean
 @Service
 public class ProductService {
 
     //add private final repo
+    private ProductRepository productRepository;
 
     //create constructor injection for repo to get dependencies
-
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     //each operation has its own method based on what website does
 
     // get - read
+    //get all products
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    //get a product by id
+    public Optional<Product> getProductById(int id) {
+        return productRepository.findById(id);
+    }
+
     // post - create
+    //add a new product
+
     // put - update
+
     // delete - remove
-
-    private ProductService
-
+    // deleet a product by ID
 
 
 }
