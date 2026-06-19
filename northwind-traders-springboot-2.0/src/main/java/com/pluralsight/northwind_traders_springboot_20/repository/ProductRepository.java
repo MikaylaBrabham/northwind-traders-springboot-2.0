23bb80853eeb19363806766eb4712c6295814c03
MikaylaBrabham <MikaylaBrabham01@gmail.com>
2026-06-19 17:48:10 -0500

    add properties

--- northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/repository/ProductRepository.java
+++ northwind-traders-springboot-2.0/src/main/java/com/pluralsight/northwind_traders_springboot_20/repository/ProductRepository.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Repository;
 
     @Repository
     public interface ProductRepository extends JpaRepository <Product, Integer>{
-       // List<Product> findByProductIdContainingIgnoreCase(Integer productId);
-       // List<Product> findByNameContainingIgnoreCase(String name);
-       // List<Product> findByCategoryContainingIgnoreCase(String category);
-       // List<Product> findByPriceContainingIgnoreCase(Double price);
+       List<Product> findByProductIdContainingIgnoreCase(Integer productId);
+       List<Product> findByNameContainingIgnoreCase(String name);
+       List<Product> findByCategoryContainingIgnoreCase(String category);
+       List<Product> findByPriceContainingIgnoreCase(Double price);
 
 
 
